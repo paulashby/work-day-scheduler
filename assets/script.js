ec1fb174ccfc9fc060383e2cb6f91dc0e6d86260
Paul Ashby <paul@primitive.co>
2022-12-12 11:43:01 +0000

    Use ul for timeslot container so we can use li elements for rows. Use textarea for memos. Update handleSave function to use .val() on textarea instead of .text(). Add custom font for save icon. Style timeslot elements.

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     // - This will allow us to add/delete local storage scheduled entries
     var scheduled = JSON.parse(localStorage.getItem("scheduled")) || []; // Empty array when not set
 
-    // Get any saved data from local storage (saved in a 'scheduled' array whose length is 9 - the number of timeblocks in the working day)
+    // Get any saved data from local storage (saved in a 'scheduled' array whose length is 9 - the number of timeblocks in the working day), provide scheduled var as handler function is in different scope
     timeblockContainer.on("click", {scheduled: scheduled}, handleSave);
 
     // Get current hour using moment().format("k") - use 24 hour notation to avoid complications of pm
@@ -19,48 +19,51 @@ $(document).ready(function(){
     // Use for loop to create timeblocks - add 9 to iterator to get correct hour - the offset from 0 to 9am
     for (var i = 0; i < WORK_DAY_LENGTH; i++) {
 
+        var rowElmt = $("<li>");
+        rowElmt.addClass("row");
+
         // Timeblock: hour in dt, content in dd - text in span and button
-        var hourLabelElmt = $("<dt>");
+        var hourLabelElmt = $("<p>");
 
         // Set hour label to ith hour of working day with am/pm suffix
         hourLabelElmt.text(moment().hour(i + WORK_DAY_START).format("hA"));
-        timeblockContainer.append(hourLabelElmt);
+        hourLabelElmt.addClass("hour");
 
         // Add any existing data from the local storage scheduled array
 
-        var hourEntryElmt = $("<dd>");
-        var hourEntryText = $("<span>");
+        var hourMemoText = $("<textarea>");
         var hourEntryBttn = $("<button>");
 
         // Make text content editable
-        hourEntryText.attr("contenteditable", "true");
+        hourMemoText.attr("contenteditable", "true");
+
+        var memoClass = "future";
+        // Apply time status class 
+        if (i + WORK_DAY_START < currentHour) {
+            memoClass = "past";
+        } else if (i + WORK_DAY_START === currentHour) {
+            memoClass = "present";
+        }
+
+        hourMemoText.addClass("memo " + memoClass);
         // Add data-index attribute to timeblock so we can store data on correct hour in scheduled array
         hourEntryBttn.attr("data-index", i);
-        hourEntryBttn.addClass("saveBtn");
+        hourEntryBttn.addClass("iconSave saveBtn");
 
         var textContent = scheduled[i];
 
         if (textContent) {
-            hourEntryText.text(textContent);
+            hourMemoText.text(textContent);
         } else {
-            hourEntryText.text("");
+            hourMemoText.text("");
         }
         hourEntryBttn.text("Save");
 
-        hourEntryElmt.append(hourEntryText);
-        hourEntryElmt.append(hourEntryBttn);
+        rowElmt.append(hourLabelElmt);
+        rowElmt.append(hourMemoText);
+        rowElmt.append(hourEntryBttn);
 
-        timeblockContainer.append(hourEntryElmt);
-
-        var timeClass = "future";
-        // Apply time status class 
-        if (i + WORK_DAY_START < currentHour) {
-            timeClass = "past";
-        } else if (i + WORK_DAY_START === currentHour) {
-            timeClass = "present";
-        }
-
-        hourLabelElmt.addClass(timeClass);
+        timeblockContainer.append(rowElmt);
     }
 
 });
@@ -69,9 +72,9 @@ function handleSave(event) {
     var clicked = $(event.target);
     var scheduled = event.data.scheduled;
 
-    if (clicked.hasClass("save-bttn")) {
+    if (clicked.hasClass("saveBtn")) {
         var scheduledIndex = parseInt(clicked.data("index"), 10);
-        var textContent = clicked.siblings("span").text();
+        var textContent = clicked.siblings(".memo").val();
         scheduled[scheduledIndex] = textContent;
         localStorage.setItem("scheduled", JSON.stringify(scheduled));
     }
